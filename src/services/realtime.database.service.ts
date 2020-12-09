3ae86c5adf8af21d91c30f4a2f1196b837e70e49
Nihal Konda <nihal.konda@sjsu.edu>
2020-12-09 09:52:48 -0800

    Built

--- src/services/realtime.database.service.ts
+++ src/services/realtime.database.service.ts
@@ -1,60 +1,60 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-interface props{
-    options?:any,
-    url?:string,
-    path?:string,
-    value?:any,
-    callback?(snapshot:firebase.database.DataSnapshot):any
+interface props {
+    options?: any,
+    url?: string,
+    path?: string,
+    value?: any,
+    callback?(snapshot: firebase.database.DataSnapshot): any
 }
 
-class RealtimeDatabase{
-    app:firebase.app.App;
+class RealtimeDatabase {
+    app: firebase.app.App;
 
-    getApp = ({options}:props) => {
+    getApp = ({ options }: props) => {
         //console.log({});
-        if(options){
-            this.app = !firebase.apps.length 
-                        ? firebase.initializeApp(options).firestore().app
-                        : firebase.app().firestore().app;
+        if (options) {
+            this.app = !firebase.apps.length
+                ? firebase.initializeApp(options).firestore().app
+                : firebase.app().firestore().app;
         }
         return this.app;
     }
 
-    getDb = ({options,url}:props) => {
+    getDb = ({ options, url }: props) => {
         //console.log({url});
-        if(url)
-            return this.getApp({options}).database(url);
-        return this.getApp({options}).database();
+        if (url)
+            return this.getApp({ options }).database(url);
+        return this.getApp({ options }).database();
     }
 
-    getPath = ({options,url,path}:props) => {
+    getPath = ({ options, url, path }: props) => {
         //console.log({url,path});
-        if(path)
-            return this.getDb({options,url}).ref(path);
-        return this.getDb({options,url}).ref();
+        if (path)
+            return this.getDb({ options, url }).ref(path);
+        return this.getDb({ options, url }).ref();
     }
 
-    pushToPath = ({options,url,path,value}:props) => {
+    pushToPath = ({ options, url, path, value }: props) => {
         //console.log({url,path,value});
-        value = value||{};
-        
-        return new Promise((resolve,reject)=>{
-            this.getPath({options,url,path}).push(value,(error)=>{
-                if(error){
+        value = value || {};
+
+        return new Promise((resolve, reject) => {
+            this.getPath({ options, url, path }).push(value, (error) => {
+                if (error) {
                     reject(error)
-                }else{
-                    resolve()
+                } else {
+                    resolve(true)
                 }
             });
         })
     }
 
-    observePath = ({options,url,path,callback}:props) => {
-        callback = callback||((snapshot:firebase.database.DataSnapshot)=>{});
-        
-        this.getPath({options,url,path}).on('child_added',(snapshot)=>{
+    observePath = ({ options, url, path, callback }: props) => {
+        callback = callback || ((snapshot: firebase.database.DataSnapshot) => { });
+
+        this.getPath({ options, url, path }).on('child_added', (snapshot) => {
             callback(snapshot);
         })
     }
